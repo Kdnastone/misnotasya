54eae7f8c9514b6cc6c2669b6c2a0138d2337967
Sofía Canizales <clsofi64@gmail.com>
2024-09-02 10:40:44 -0500

    add: back-end

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+window.addEventListener("load", iniciar)
 
+//Función que obtiene el botón y válida el evento de 'click'
+function iniciar() {
+  let botonGuardarNota = document.getElementById("guardarNota")
+  botonGuardarNota.addEventListener("click", guardarNota)
+}
+
+//Función para guardar cada nota
+function guardarNota() {
+    let nota = document.getElementById("noteText")
+    let espacioNota = document.getElementById("notes") 
+    let notaArticle = document.createElement("article")  //creareamos un <article> donde guardaremos por independiente cada nota
+    notaArticle.textContent = nota.value 
+    espacioNota.appendChild(notaArticle) 
+    nota.value = "" 
+  }
